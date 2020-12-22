3ae8c59a5373ad5b9027a8c0b9b098b6cef9ee92
Cyrrril <73851084+Cyrrril@users.noreply.github.com>
2020-12-22 17:08:30 +0100

    Add files via upload

--- plavon.py
+++ plavon.py
@@ -1,5 +1,5 @@
-api_key = open("apikey.txt", "r")
-api_secret = open("secretkey.txt", "r")
+api_key = "quiquechose"
+api_secret = "quiquechose"
 
 from binance.client import Client
 from binance.enums import *
@@ -9,20 +9,46 @@ from sys import exit
 from binance.client import Client
 from binance.websockets import BinanceSocketManager
 from twisted.internet import reactor
+import math
 client = Client(api_key, api_secret)
 
-for i in range (2):
+ticks = {}
+for filt in client.get_symbol_info('BTCUSDT')['filters']:
+    if filt['filterType'] == 'LOT_SIZE':
+        ticks['BTC'] = filt['stepSize'].find('1') - 2
+        break
+
+for filt in client.get_symbol_info('ROSEUSDT')['filters']:
+    if filt['filterType'] == 'LOT_SIZE':
+        ticks['ROSE'] = filt['stepSize'].find('1') - 2
+        break
+
+for filt in client.get_symbol_info('ROSEBTC')['filters']:
+    if filt['filterType'] == 'LOT_SIZE':
+        ticks['ROSEBTC'] = filt['stepSize'].find('1') - 2
+        break
+
+balanceBTC = client.get_asset_balance(asset='BTC')
+balanceUSDT = client.get_asset_balance(asset='USDT')
+balanceROSE = client.get_asset_balance(asset='ROSE')
+#print("UN GAP SILVOUPLAIT")
+#print("ci kwa ca -> ", ticks)
+#print(math.floor(378.9995 * 10**ticks['BTC']) / float(10**ticks['BTC']))
+for i in range (1):
     # Recuperation des prix
     usdt_b_p = float((client.get_avg_price(symbol='BTCUSDT'))["price"])
     usdt_r_p = float(client.get_avg_price(symbol='ROSEUSDT')["price"])
     b_r_p = float((client.get_avg_price(symbol='ROSEBTC'))["price"])
     gap = ((((500/usdt_b_p/b_r_p*usdt_r_p) - 500)/500)*100)
+
+    usdt_b_p_2 = float((client.get_avg_price(symbol='BTCUSDT'))["price"])
+    usdt_r_p_2 = float(client.get_avg_price(symbol='ROSEUSDT')["price"])
+    b_r_p_2 = float((client.get_avg_price(symbol='ROSEBTC'))["price"])
+    gap_2 = ((((500/usdt_b_p_2/b_r_p_2*usdt_r_p_2) - 500)/500)*100)
+
     print("Gap aquoitigal = ", gap)
-    if (abs(gap) > 0.0):
+    if (abs(gap) > 0.32 and abs(gap_2) > 0.32):
         # recuperation des balances
-        balanceBTC = client.get_asset_balance(asset='BTC')
-        balanceUSDT = client.get_asset_balance(asset='USDT')
-        balanceROSE = client.get_asset_balance(asset='ROSE')
 
         if gap > 0:
 
@@ -33,22 +59,56 @@ for i in range (2):
             balanceUSDT = client.get_asset_balance(asset='USDT')
 
             # Buy BTC with USDT
-            client.order_market_buy(symbol='BTCUSDT', quoteOrderQty=round(float(balanceUSDT["free"]) - float(10**(-8)) ,8))
+            client.order_market_buy(symbol='BTCUSDT', quoteOrderQty=math.floor(float(balanceUSDT["free"]) * 10**ticks['BTC']) / float(10**ticks['BTC']))
             #print("Oui : ",round(float(float(balanceUSDT["free"])/usdt_b_p) - float(10**(-5)),5))
             #print(" Buy BTC with USDT \n")
             # Buy ROSE with BTC
+            
+            
+            print("Buy ROSE with BTC")
 
             balanceBTC = client.get_asset_balance(asset='BTC')
-            print(balanceBTC)
-            client.order_market_buy(symbol='ROSEBTC', quoteOrderQty=round(float(balanceBTC["free"]) - float(10**(-8)) ,8))
-            #print(" Buy ROSE with BTC \n")
+            balanceUSDT = client.get_asset_balance(asset='USDT')
+            balanceROSE = client.get_asset_balance(asset='ROSE')
+            print (balanceBTC)
+            print (balanceUSDT)
+            print (balanceROSE)
+
+
+            sleep(0.1)
+
+            balanceBTC = client.get_asset_balance(asset='BTC')
+            #print(balanceBTC)
+            client.order_market_buy(symbol='ROSEBTC', quoteOrderQty=math.floor(float(balanceBTC["free"]) * 10**ticks['BTC']) / float(10**ticks['BTC']))
+            
+            print(" Buy ROSE with BTC \n")
+            balanceBTC = client.get_asset_balance(asset='BTC')
+            balanceUSDT = client.get_asset_balance(asset='USDT')
+            balanceROSE = client.get_asset_balance(asset='ROSE')
+            print (balanceBTC)
+            print (balanceUSDT)
+            print (balanceROSE)
+
+
+
+           
             # Sell ROSE for USDT
 
+            sleep(0.5)
+
             balanceROSE = client.get_asset_balance(asset='ROSE')
-            qt_Rose_Usdt = round(float(balanceROSE["free"])*usdt_r_p - float(10**(-8)),8)
-            print(float(balanceROSE["free"]))
-            client.order_market_sell(symbol='ROSEUSDT', quantity = qt_Rose_Usdt)
-            #print("Sell ROSE for USDT")
+            #qt_Rose_Usdt = round(float(balanceROSE["free"])*usdt_r_p - float(10**(-8)),8)
+            #print(float(balanceROSE["free"]))
+            client.order_market_sell(symbol='ROSEUSDT', quantity = math.floor(float(balanceROSE["free"]) * 10**ticks['BTC']) / float(10**ticks['BTC']))
+            print("Sell ROSE for USDT")
+
+            balanceBTC = client.get_asset_balance(asset='BTC')
+            balanceUSDT = client.get_asset_balance(asset='USDT')
+            balanceROSE = client.get_asset_balance(asset='ROSE')
+            print (balanceBTC)
+            print (balanceUSDT)
+            print (balanceROSE)
+
         else:
             
             #Vars Sens 1
@@ -56,22 +116,48 @@ for i in range (2):
             balanceUSDT = client.get_asset_balance(asset='USDT')
 
             # Buy ROSE with USDT
-            client.order_market_buy(symbol='ROSEUSDT', quoteOrderQty=round(float(balanceUSDT["free"]) - float(10**(-8)) ,8))
+            client.order_market_buy(symbol='ROSEUSDT', quoteOrderQty=math.floor(float(balanceUSDT["free"]) * 10**ticks['BTC']) / float(10**ticks['BTC']))
             #print("Pythoun : ", round(float(float(balanceUSDT["free"])/usdt_r_p) - float(10**(-5)),5))
-            #print("Buy ROSE with USDT")
+            print("Buy ROSE with USDT")
             # Sell ROSE for BTC
 
+            balanceBTC = client.get_asset_balance(asset='BTC')
+            balanceUSDT = client.get_asset_balance(asset='USDT')
             balanceROSE = client.get_asset_balance(asset='ROSE')
-            qt_Usdt_Rose = round(float(balanceUSDT["free"])/usdt_r_p - float(10**(-5)),5)
-            client.order_market_sell(symbol='ROSEBTC', quantity=qt_Usdt_Rose)
-            #print("Sell ROSE for BTC")
+            print (balanceBTC)
+            print (balanceUSDT)
+            print (balanceROSE)
+
+            sleep(0.1)
+
+            balanceROSE = client.get_asset_balance(asset='ROSE')
+            #qt_Usdt_Rose = round(float(balanceUSDT["free"])/usdt_r_p - float(10**(-5)),5)
+            client.order_market_sell(symbol='ROSEBTC', quantity=math.floor(float(balanceROSE["free"]) * 10**3) / float(10**3))
+            print("Sell ROSE for BTC")
             # Sell BTC for USDT
 
             balanceBTC = client.get_asset_balance(asset='BTC')
-            qt_Usdt_Btc = round(float(balanceBTC["free"])*usdt_b_p - float(10**(-5)),5)
-            qt_Rose_Btc = round(float(balanceROSE["free"])*b_r_p - float(10**(-8)),8)
-            client.order_market_sell(symbol='BTCUSDT', quantity = qt_Rose_Btc)
-            #print("Sell BTC for USDT")
+            balanceUSDT = client.get_asset_balance(asset='USDT')
+            balanceROSE = client.get_asset_balance(asset='ROSE')
+            print (balanceBTC)
+            print (balanceUSDT)
+            print (balanceROSE)
+
+            sleep(0.5)
+
+            balanceBTC = client.get_asset_balance(asset='BTC')
+            #qt_Usdt_Btc = round(float(balanceBTC["free"])*usdt_b_p - float(10**(-5)),5)
+            #qt_Rose_Btc = round(float(balanceROSE["free"])*b_r_p - float(10**(-8)),8)
+            client.order_market_sell(symbol='BTCUSDT', quantity = math.floor(float(balanceBTC["free"]) * 10**ticks['BTC']) / float(10**ticks['BTC']))
+            print("Sell BTC for USDT")
+
+            balanceBTC = client.get_asset_balance(asset='BTC')
+            balanceUSDT = client.get_asset_balance(asset='USDT')
+            balanceROSE = client.get_asset_balance(asset='ROSE')
+            print (balanceBTC)
+            print (balanceUSDT)
+            print (balanceROSE)
+
     print("-------------------------------------------")
 """
 SENS 1 ROSE BTC
